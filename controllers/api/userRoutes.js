65e0289c60ad2b9b55195228ac604417bb6fe456
rgonsahn <rgonsahn@yahoo.com>
2022-12-02 15:13:31 -0500

    created homepage route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,6 @@
 const router=require('express').Router()
 
-
-
+       
 
 
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,33 @@
 // only for get routes
 const router=require('express').Router()
+const { Post, User,Comment } = require('../models');
 
 
-
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const posts = postData.map((post) => posts.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        post, 
+        loggedIn: req.session.loggedIn 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
 
 
