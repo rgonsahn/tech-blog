192023296d9d976d75e36ca8853edb6ec69e332c
rgonsahn <rgonsahn@yahoo.com>
2022-12-04 07:42:19 -0500

    created the user route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,36 +2,29 @@
 const router=require('express').Router()
 const { Post, User,Comment } = require('../models');
 
-
 router.get('/', async (req, res) => {
-    try {
-      // Get all projects and JOIN with user data
-        await Post.findAll({
-          attributes:[
-            'title',
-            'content',
-          ],
-
-        include: [
-          {
-            model: User,
-            attributes: ['name','id','email','password'],
-            include:{
-              model: Comment,
-              attributes:['content']
-            }
-          },
+  try {
+    // Get all projects and JOIN with user data
+      await Post.findAll({
+        attributes:[
+          'title',
+          'content',
         ],
-      }) .then(postData =>{
-        const posts = postData.map((post) => posts.get({ plain: true }));
 
-      res.render('homepage', {  post,loggedIn: req.session.loggedIn});
-      })
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
+      include: [
+        {
+          model: User
+        },
+      ],
+    }) .then(postData =>{
+      const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homePage', {posts});
+    })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
--- server.js
+++ server.js
@@ -31,15 +31,23 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+const hbs = exphbs.create();
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+
+
+
+
+
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
